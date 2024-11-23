095d518b29385377a3ea06be2b37ea1e7f5d9ca5
Jack <jack20220723@gmail.com>
2024-11-23 20:26:59 +0800

    PoC implementation

---
+++ README.md
@@ -0,0 +1,59 @@
+VK 45.01
+========
+
+![Node Version][node version badge]
+[![GitHub workflow status badge][GitHub workflow status badge]][GitHub workflow status URL]
+[![Apache License][Apache License Badge]][Apache License, Version 2.0]
+
+__VK 45.01__ is a companion [web app](https://vk4501.qubitpi.org/) hosting extracted subtitles of selected movies using [Tiger](https://tiger.qubitpi.org)
+
+Selected Movies
+---------------
+
+### Generation War (German)
+
+#### Movie Source
+
+- [Generation War E01 (part 1)](https://www.dailymotion.com/video/x6y1zfs)
+- [Generation War E01 (part 1)](https://www.dailymotion.com/video/x6y2fdh)
+- [Generation War E02 (part 1)](https://www.dailymotion.com/video/x6y64ha)
+- [Generation War E02 (part 2)](https://www.dailymotion.com/video/x6y67ir)
+- [Generation War E03 (part 1)](https://www.dailymotion.com/video/x6ya2qj)
+- [Generation War E03 (part 2)](https://www.dailymotion.com/video/x6ya6yg)
+
+#### Subtitles
+
+- [EP1 - German subtitles.srt](./EP1%20-%20German%20subtitles.srt) (Work in Progress using [QubitPi/tiger](https://tiger.qubitpi.org/))
+
+Development
+-----------
+
+> [!IMPORTANT]
+>
+> VK 45.01 requires Node 18 or above
+
+### Starting Locally
+
+To start Wilhelm locally:
+
+```console
+git clone git@github.com:QubitPi/VK-45.01.git
+cd VK-45.01
+yarn
+yarn start
+```
+
+Then visit [localhost:3000](http://localhost:3000/)
+
+License
+-------
+
+The use and distribution terms for [VK-45.01]() are covered by the [Apache License, Version 2.0].
+
+[Apache License Badge]: https://img.shields.io/badge/Apache%202.0-F25910.svg?style=for-the-badge&logo=Apache&logoColor=white
+[Apache License, Version 2.0]: https://www.apache.org/licenses/LICENSE-2.0
+
+[GitHub workflow status badge]: https://img.shields.io/github/actions/workflow/status/QubitPi/wilhelm/ci-cd.yaml?branch=master&style=for-the-badge&logo=github&logoColor=white&label=CI/CD
+[GitHub workflow status URL]: https://github.com/QubitPi/wilhelm/actions/workflows/ci-cd.yaml
+
+[node version badge]: https://img.shields.io/badge/NODE-18-339933?logo=Node.js&logoColor=white&labelColor=66cc33&style=for-the-badge
---
+++ src/App.tsx
@@ -0,0 +1,130 @@
+/**
+ * Copyright Jiaqi Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, {useState} from 'react';
+import './App.css';
+import PlayingVideo from "./components/PlayingVideo";
+import PlayList from "./components/PlayingList";
+
+function App() {
+  const videos: {title: string, duration: string, transcript: JSX.Element}[] = [
+    {
+      "title": "Generation War - Party",
+      "duration": "1:49",
+      "transcript": <div>
+        Wir waren fünf. Fünf Freunde. Wir wollten noch einmal gebührend voneinander Abschied nehmen, bevor sich unsere
+        Wege trennten. Wir waren im selben Viertel aufgewachsen und kannten uns, seit wir Kinder waren. Wir hätten nicht
+        unterschiedlicher sein können. Und doch waren wir seit der Zeit Freunde, die zusammenhielten. Charly, unser
+        Nesthäkchen, will seit Kriegsbeginn den Dienst fürs Vaterland leisten. Sie wird als Krankenschwester in einem
+        Lazarett arbeiten, das hinter der Front meiner Einheit folgt. Viktor Goldstein. Sein Vater war Unteroffizier im
+        Ersten Weltkrieg und später ein angesehener Schneider. Viktor sollte das Geschäft übernehmen, bevor es in der
+        Reichskristallnacht in Scherben zerfiel. Das ist seine Freundin Greta. Die beiden sind unzertrennlich. Greta
+        interessiert es nicht, dass ihre Beziehung zur Victor als Rassenschande gilt. Sie wird einmal ein Star werden.
+        Die neue Marlene Dietrich. Mein kleiner Bruder Friedhelm. Der Bücherwurm, der Rimbaud und Jünger liebt. Ich
+        bewundere ihn für seine Klugheit und seinen scharfen Witz. Für ihn ist es das erste Mal. Und hier bin ich,
+        Wilhelm Winter. Leutnant der Windhund-Kompanie. Einsatz in Polen und Frankreich. Und morgen geht's Richtung
+        Russland. <br/>
+        <br/>
+        - Der Krieg wird schon noch einen Mann aus ihm machen. <br/>
+        - Glaub mir, der Krieg wird nur das Schlechteste in uns zum Vorschein bringen. <br/>
+        - Ich würde gern mitmachen, wie mein Vater 1913, aber wenn ihr verliert, heißt es wieder Wir waren schon. <br/>
+        - Das ist eben kein Land für Schwächlinge und Volksfremde. <br/>
+        - Jeder muss Opfer bringen. <br/>
+      </div>
+    },
+    {
+      "title": "Generation War - Party Pray",
+      "duration": "1:02",
+      "transcript": <div>
+        - Greta, wir müssen los. <br/>
+        - Jetzt? <br/>
+        - Charly wollte noch ... <br/>
+        - Das kann warten. <br/>
+        - Spätestens Weihnachten ist der Krieg vorbei. <br/>
+        - Ja, lass uns schwören. Weihnachten sind wir alle wieder hier. Hier in diesem Raum. Lebend. <br/>
+        - Weihnachten in Berlin. <br/>
+        - Weihnachten in Berlin. <br/>
+        - Weihnachten in Berlin. <br/>
+        - Wartet, ich mach noch ein Foto. Na los, stell dich dann dort nach hinten ans Regal. Na, kommt. <br/>
+        - Hier? <br/>
+        - Ja, das ist gut. Nimm einen Stuhl, einen Hocker, irgendwas. Friedhelm, komm nach vorne. Nimm Greta. <br/>
+        Wir waren fünf Freunde. Wir waren jung und wir wussten, dass uns die Zukunft gehören würde. Die ganze Welt lag
+        vor uns. Wir mussten nur zugreifen. Wir waren unsterblich. Wir sollten es bald besser wissen.
+      </div>
+    },
+    {
+      "title": "Generation War - Field",
+      "duration": "1:30",
+      "transcript": <div>
+        <p>
+          10. Juli 1941. Dies wird ein anderer Krieg werden als alle zuvor. Es ist eine Lust vorzustoßen. Der Rausch
+          der Geschwindigkeit. Immer weiter und weiter und weiter.
+        </p>
+        <br/>
+        <p>
+          Die Endlosigkeit des Raumes. Landschaften, wie wir sie noch nie gesehen hatten. Felder bis zum Horizont. Der
+          Himmel so weit und blau, als wollte Gott uns mit der Schönheit seiner Schöpfung blenden.
+        </p>
+        <br/>
+        <p>
+          - Ist das nicht großartig? <br/>
+          - Im nächsten Jahr werden hier überall deutsche Höfe stehen. <br/>
+        </p>
+        <br/>
+        <p>
+          Meine Männer schlagen sich tadellos. Bei jedem Einsatz sind sie ganz vorne dabei. Alle bis auf Friedhelm, den
+          alle für
+          einen Feigling halten, der mich jeden Tag provoziert und für den ich mich schäme.
+        </p>
+        <br/>
+
+        - Was meint ihr, wie weit ist es noch bis Moskau? <br/>
+        - So um die 700 Kilometer. <br/>
+        - Na, lasst uns durchfahren. Nicht umsonst sehen wir die Windhunde. <br/>
+        - Frühstück auf dem Roten Platz mit Wodka und Kaviar. <br/>
+        - Und wie willst du dann ein Held werden? <br/>
+        - Kein einziger Russen erwischt, der will schon den Krieg gewonnen haben. <br/>
+        - Genau. <br/>
+        - Na, wenn sich die Russen immer so Vorteile ergeben, soll ich Gefangene abknallen. <br/>
+        - Winter hier will mir denen ja liebe Gedichte vortragen. <br/>
+        - Koch, wie viel hast du? <br/>
+        - Koch hat bestimmt schon zehn. <br/>
+        - Dann schreibt Freitag seiner Frau morgen 'ne Karte vom Roten Platz. <br/>
+      </div>
+    }
+  ]
+
+  const [playingTitle, setPlayingTitle] = useState<string>(videos[0].title)
+
+  return (
+      <div className="container">
+        <div className="playingVideoSection">
+          <PlayingVideo
+              videoTitle={playingTitle}
+              transcript={videos.filter(video => video.title === playingTitle)[0].transcript}
+          />
+        </div>
+        <div className="playListSection">
+          <PlayList
+              playingTitle={playingTitle}
+              videos={videos}
+              listItemOnClick={(video) => setPlayingTitle(video.title)}
+          />
+        </div>
+      </div>
+  );
+}
+
+export default App;
